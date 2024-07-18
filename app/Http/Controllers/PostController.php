aa1ede599975960116542aed261cbbf5abe175e3
ChiLo <pyha1994@yandex.ru>
2024-07-18 21:48:52 +0300

    up project

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ class PostController extends Controller
 {
     public function index(): string
     {
-        return view('posts');
+        return view('posts.index');
     }
 
     public function create()
