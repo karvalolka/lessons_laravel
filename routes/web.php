c28cb36d9b14588b30ae47efe087b463e0d10b3b
ChiLo <pyha1994@yandex.ru>
2024-07-25 22:48:16 +0300

    up project

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactsController;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\Post\CreateController;
+use App\Http\Controllers\Post\DestroyController;
+use App\Http\Controllers\Post\EditController;
+use App\Http\Controllers\Post\IndexController;
+use App\Http\Controllers\Post\ShowController;
+use App\Http\Controllers\Post\StoreController;
+use App\Http\Controllers\Post\UpdateController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,14 +29,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'index'])->name('post.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
+Route::group(['namespace' => 'Post'], function (){
+    Route::get('/posts', [IndexController::class])->name('post.index');
+    Route::get('/posts/create', [CreateController::class])->name('post.create');
 
-Route::post('/posts', [PostController::class, 'store'])->name('post.store');
-Route::get('/posts/{post}', [PostController::class, 'show'])->name('post.show');
-Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
-Route::patch('/posts/{post}', [PostController::class, 'update'])->name('post.update');
-Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('post.delete');
+    Route::post('/posts', [StoreController::class])->name('post.store');
+    Route::get('/posts/{post}', [ShowController::class])->name('post.show');
+    Route::get('/posts/{post}/edit', [EditController::class])->name('post.edit');
+    Route::patch('/posts/{post}', [UpdateController::class])->name('post.update');
+    Route::delete('/posts/{post}', [DestroyController::class])->name('post.delete');
+});
+
+
+
 
 Route::get('/posts/update', [PostController::class, 'update']);
 Route::get('/posts/delete', [PostController::class, 'delete']);
